98661ee85172cc1d688c7fc9adf1d2d5d25053a3
EC2 Default User <0311.hnmyuya@gmail.com>
2024-02-18 08:18:50 +0000

    コントローラ名修正

--- app/controllers/public/trainer/confirmations_controller.rb
+++ app/controllers/public/trainer/confirmations_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Admin::Trainer::ConfirmationsController < Devise::ConfirmationsController
+class Public::Trainer::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation/new
   # def new
   #   super
