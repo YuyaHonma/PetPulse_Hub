79bc7fc82ab31c3545e3eb11f5deb53d13f3a885
EC2 Default User <0311.hnmyuya@gmail.com>
2024-02-27 08:20:44 +0000

    飼い主トップページ修正

--- app/controllers/public/owner/homes_controller.rb
+++ app/controllers/public/owner/homes_controller.rb
@@ -2,7 +2,7 @@ class Public::Owner::HomesController < ApplicationController
     
   def top
     @posts = Post.order(created_at: :desc).limit(8) # 新着投稿一覧を取得
-    @trainers = Trainer.limit(6) # トレーナー一覧を取得
+    @trainers = Trainer.limit(8) # トレーナー一覧を取得
     @categorys = Category.limit(6) # カテゴリー一覧を取得
   end
 
