d8dc6313ecd4d9e8257e4c092d8d158c38bfbe08
EC2 Default User <0311.hnmyuya@gmail.com>
2024-02-27 06:09:24 +0000

    メッセージ内容のレイアウト見直し

--- app/controllers/public/trainer/homes_controller.rb
+++ app/controllers/public/trainer/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::Trainer::HomesController < ApplicationController
      
   def top
-    @posts = Post.order(created_at: :desc).limit(6)
-    @trainers = Trainer.limit(6) # トレーナー一覧を取得
+    @posts = Post.order(created_at: :desc).limit(8)
+    @trainers = Trainer.limit(8) # トレーナー一覧を取得
   end
 end
